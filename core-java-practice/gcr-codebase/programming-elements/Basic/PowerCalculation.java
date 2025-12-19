15dd18b51cb470f95a18a9b07caa2080b758a96d
shyamparashar42@gmail.com <shyamparashar42@gmail.com>
2025-12-19 11:32:02 +0530

    refactored naming convension

---
+++ core-java-practice/gcr-codebase/programming-elements/Basic/PowerCalculation.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PowerCalculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the base number: ");
+        double base = sc.nextDouble();
+        System.out.print("Enter the exponent: ");
+        double exponent = sc.nextDouble();
+        double result = Math.pow(base, exponent);
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+        sc.close();
+    }
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/Basic/VolumeCylinder.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class VolumeCylinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the radius of the cylinder:");
+        double radius = scanner.nextDouble();
+        System.out.println("Enter the height of the cylinder:");    
+        double height = scanner.nextDouble();
+        double volume = 3.14 * radius * radius * height;
+        System.out.println("The volume of the cylinder is: " + volume);
+        scanner.close();
+    }
+}
