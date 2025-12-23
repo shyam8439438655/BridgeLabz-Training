29225c3330eabfa84037b71833e6ebb9b3995062
shyamparashar42@gmail.com <shyamparashar42@gmail.com>
2025-12-23 16:30:10 +0530

    [Shyam Parashar] add reveiw

---
+++ core-java-practice/gcr-codebase/programming-elements/arrays/EvenOddCountByArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class EvenOddCountByArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] numbers = new int [n];
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                evenCount++;
+            }
+             else {
+                oddCount++;
+            }
+        }
+        System.out.println("Number of even integers: " + evenCount);
+        System.out.println("Number of odd integers: " + oddCount);
+        scanner.close();
+    }
+}
